558555a50e9f1bdb0a460ac4da8a91f2539930f9
Facundo Martínez <fnmartinez88@gmail.com>
2021-10-03 13:21:00 +0200

    master: 	* Extracting basic functionality into a common base class

---
+++ didomi/base.py
@@ -0,0 +1,68 @@
+import logging
+from abc import ABC, abstractmethod
+from datetime import datetime
+from typing import Tuple
+
+from pyspark.sql import SparkSession, DataFrame
+
+
+class AbstractBaseDidomiJob(ABC):
+    def __init__(self, spark: SparkSession, input_path: str, input_partitions: Tuple[datetime], output_path: str):
+        self.log = logging.getLogger(self.__class__.__name__)
+        self.spark = spark
+        self.input_path = input_path
+        if input_partitions is None or isinstance(input_partitions, tuple) and len(input_partitions) == 0:
+            self.input_partitions = None
+        else:
+            self.input_partitions = input_partitions
+        self.output_path = output_path
+
+    @abstractmethod
+    def do_extraction(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def do_work(self):
+        raise NotImplementedError()
+
+    @abstractmethod
+    def do_save(self):
+        raise NotImplementedError()
+
+    @property
+    @abstractmethod
+    def input_data(self) -> DataFrame:
+        raise NotImplementedError()
+
+    @input_data.setter
+    @abstractmethod
+    def input_data(self, x: DataFrame):
+        raise NotImplementedError()
+
+    def _partition_datetime_to_str(self, partition: datetime):
+        return partition.strftime('%Y-%m-%d-%H')
+
+    def _filter_by_partitions(self):
+        if self.input_partitions:
+            self.log.info('Filtering by partitions %s', str(self.input_partitions))
+            for partition in self.input_partitions:
+                partition_filter = self.input_data['datehour'] == self._partition_datetime_to_str(partition)
+                self.input_data = self.input_data.filter(partition_filter)
+
+    def _extract(self):
+        self.log.info('Extracting data from %', self.input_path)
+        self.do_extraction()
+        self._filter_by_partitions()
+
+    def _work(self):
+        self.log.info("Executing job's main process")
+        self.do_work()
+
+    def _save(self):
+        self.log.info('Saving data')
+        self.do_save()
+
+    def execute(self):
+        self._extract()
+        self._work()
+        self._save()
--- didomi/etls.py
+++ didomi/etls.py
@@ -5,7 +5,7 @@ from pyspark.sql import functions as f
 from pyspark.sql import SparkSession, DataFrame, Column
 from pyspark.sql.types import StructType
 
-from didomi import schemas
+from didomi import base, schemas
 
 
 def flatten_schema(schema: StructType, prefix: str = None) -> List[Column]:
@@ -19,42 +19,32 @@ def flatten_schema(schema: StructType, prefix: str = None) -> List[Column]:
     return columns
 
 
-class ConsentNormalization:
+class ConsentNormalization(base.AbstractBaseDidomiJob):
 
     def __init__(self, spark: SparkSession, input_path: str, input_partitions: Tuple[datetime], output_path: str):
-        self.spark = spark
-        self.input_path = input_path
-        if input_partitions is None or isinstance(input_partitions, tuple) and len(input_partitions) == 0:
-            self.input_partitions = None
-        else:
-            self.input_partitions = input_partitions
-        self.output_path = output_path
+        super().__init__(spark, input_path, input_partitions, output_path)
         self.raw_consents: DataFrame = None
         self.normalized_consents: DataFrame = None
 
-    def _partition_datetime_to_str(self, partition: datetime):
-        return partition.strftime('%Y-%m-%d-%H')
-
-    def extract(self):
+    def do_extraction(self):
         self.raw_consents = self.spark.read \
             .option('basePath', self.input_path) \
             .json(self.input_path, schema=schemas.raw_consent_schema)
-        if self.input_partitions:
-            for partition in self.input_partitions:
-                partition_filter = self.raw_consents['datehour'] == self._partition_datetime_to_str(partition)
-                self.raw_consents = self.raw_consents.filter(partition_filter)
 
-    def normalize(self):
+    def do_work(self):
         self.normalized_consents = self.raw_consents \
             .select(flatten_schema(schemas.raw_consent_schema)) \
             .drop_duplicates()
 
-    def save(self):
+    def do_save(self):
         self.normalized_consents.write.parquet(self.output_path,
                                                mode='overwrite',
                                                partitionBy=['datehour'])
 
-    def execute(self):
-        self.extract()
-        self.normalize()
-        self.save()
+    @property
+    def input_data(self) -> DataFrame:
+        return self.raw_consents
+
+    @input_data.setter
+    def input_data(self, x: DataFrame):
+        self.raw_consents = x
